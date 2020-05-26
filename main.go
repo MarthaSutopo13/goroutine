420aae70d6bfa8b0d3b6b9f2201794df9d073476
Martha Sutopo <martha.sutopo@gmail.com>
2020-05-26 15:51:24 +0700

    add sync and defer

--- main.go
+++ main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+	wg.Add(3)
 	go CetakNama("Hi")
 	go CetakNama("Hello")
+	// wg.Wait()
 
-	var i string
-	fmt.Scanln(&i)
+	// wg.Add(1)
+	go CetakNama("What's up?")
+	wg.Wait()
 
-	fmt.Println(i)
 }
 
 func CetakNama(s string) {
 	for i := 0; i < 5; i++ {
 		fmt.Println(s)
-		// time.Sleep(time.Second)
+		time.Sleep(time.Millisecond * 100)
 	}
+	defer wg.Done()
 }
